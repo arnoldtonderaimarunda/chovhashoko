1a8c5b9bb2d8d76c1b391ba4f0eae2d83468d439
Arnold™ <hello@workflowautomation.expert>
2023-03-04 22:12:05 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -91,7 +91,7 @@ circles.map((circle, index) => {
         ...circle,
         positionIndex: index,
         fadeSpeed: timeControl.circlesFadeSpeed,
-        clickHandler: (circle) => console.log('Clicking ' + ${circle})
+        clickHandler: (circle) => console.log(`Clicking ${circle}`)
     });
     circleObject.render();
     circleObjects.push(circleObject);
